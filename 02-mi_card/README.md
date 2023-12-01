ad77da8cfa839dce02799898dab442c075cdb1f6
Adhamxiii <adhamxiii22@gmail.com>
2023-12-01 16:29:54 +0200

    Lesson 2

---
+++ 02-mi_card/README.md
@@ -0,0 +1,42 @@
+# Lesson 2: Creating a Personal Profile App
+
+In this second lesson of my Flutter learning journey, I built a personal profile app that showcases my information using various Flutter widgets and layouts.
+
+## What I Learned
+
+- **StatelessWidget**: I created a `MyApp` class that extends `StatelessWidget`, which represents the core of the app's user interface.
+
+- **Material Design**: I utilized the `MaterialApp` and `Scaffold` widgets to follow Material Design guidelines for the app's structure and layout.
+
+- **SafeArea**: I wrapped the app's content in a `SafeArea` widget to ensure that it's displayed within the safe boundaries of the device.
+
+- **Column Layout**: I organized the UI elements in a vertical layout using the `Column` widget, which allowed me to stack multiple widgets on top of each other.
+
+- **CircleAvatar**: I used the `CircleAvatar` widget to display a circular profile picture with a background image.
+
+- **Text Styling**: I customized the styling of text using the `TextStyle` class to set different fonts, sizes, colors, and font weights.
+
+- **Divider**: I added a horizontal line using the `Divider` widget to separate different sections of the profile.
+
+- **Card and ListTile**: I used `Card` widgets combined with `ListTile` widgets to display contact information in a visually appealing way.
+
+- **Icon**: I included icons from the `Icons` class to represent phone and email contacts.
+
+## Preview
+
+<img src="screenshots/app_preview.png" alt="App Preview" width="300">
+
+## Next Steps
+
+In the next lesson, I'll be diving deeper into Flutter development, exploring more advanced concepts, and enhancing the interactivity of my apps. Stay tuned for more exciting updates!
+
+## Contact Information
+
+If you have any questions or need assistance, you can reach out to the project author:
+
+- Email: [Adham Nasser](mailto:adhamxiii22@gmail.com)
+- LinkedIn: [Adham Nasser](https://www.linkedin.com/in/adhamxiii/)
+- GitHub: [Adham Nasser](https://github.com/Adhamxiii)
+
+I'd love to hear your thoughts and feedback!
+
---
+++ 02-mi_card/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile-pic.png'),
+              ),
+              const Text(
+                'Adham Nasser',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                    fontFamily: "Source Sans Pro",
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+               SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                    leading: const Icon(Icons.phone, color: Colors.teal),
+                    title: Text(
+                      '01277346643',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                      ),
+                    )),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: const Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    'adhamxiii10@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
